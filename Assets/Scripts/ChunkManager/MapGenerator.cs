8a9332e022922a0eb8244a98db7906b72115aa0b
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-04-18 17:53:38 +0900

    [Fix] 맵 생성 간 타일 규칙 설정, 스폰 관련 로직 생성
    - 맵 생성 간, 기둥, 게이트와 같은 특정 규칙에 따라 생성될 오브젝트에 대한 규칙 설정
    - 플레이어 및 적의 스폰 위치를 얻을 수 있는 메서드 생성
    - 청크 매니저 싱글톤 설정

--- Assets/Scripts/ChunkManager/MapGenerator.cs
+++ Assets/Scripts/ChunkManager/MapGenerator.cs
@@ -1,21 +1,49 @@
 using System.Collections.Generic;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
-public static class MapGenerator
+public enum TileType
 {
-    public enum TileType
+    Empty,
+    Floor,
+    Wall,
+    Pillar,
+    Celling,
+}
+
+public class MapGenerator: MonoBehaviour
+{
+    public GameObject FloorPrefab;
+    public GameObject WallPrefab;
+    public GameObject PillarPrefab;
+    public GameObject CeilingPrefab;
+    public GameObject GatePrefab;
+
+    public Vector3 WallOffset;
+    public Vector3 PillarOffset;
+    public Vector3 CeilingOffset;
+    public Vector3 GateOffset;
+
+    public static MapGenerator Instance;
+
+    private void Awake()
     {
-        Empty,
-        Floor,
-        Wall
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
-    public static void GenerateChunk(RectInt bounds, Transform parent, GameObject floorPrefab, GameObject wallPrefab, int minRoomSize)
+    public void GenerateChunk(RectInt bounds, Transform parent, int minRoomSize, out List<Vector3> floorPosData)
     {
         int width = bounds.width;
         int height = bounds.height;
-        TileType[,] mapData = new TileType[width, height];
+        var mapData = new TileType[width, height];
+        floorPosData = new List<Vector3>();
 
         BSPNode root = new BSPNode(new RectInt(0, 0, width, height));
         root.Split(minRoomSize);
@@ -37,6 +65,7 @@ public static class MapGenerator
         // 복도 연결
         ConnectRooms(root, mapData);
 
+
         // 바닥 생성
         for (int x = 0; x < width; x++)
         {
@@ -45,37 +74,12 @@ public static class MapGenerator
                 if (mapData[x, y] == TileType.Floor)
                 {
                     Vector3 pos = new Vector3(bounds.x + x, 0, bounds.y + y);
-                    GameObject floor = GameObject.Instantiate(floorPrefab, pos, Quaternion.identity, parent);
+                    Transform floor = GameObject.Instantiate(FloorPrefab, pos, Quaternion.identity, parent).transform;
+                    floorPosData.Add(floor.position);
+                    GameObject.Instantiate(CeilingPrefab, pos + CeilingOffset, Quaternion.identity, parent);
                 }
             }
         }
-
-        //Vector2Int[] directions = {
-        //    new Vector2Int(1, 0), new Vector2Int(-1, 0),
-        //    new Vector2Int(0, 1), new Vector2Int(0, -1)
-        //};
-
-        //for (int x = 1; x < width - 1; x++)
-        //{
-        //    for (int y = 1; y < height - 1; y++)
-        //    {
-        //        if (mapData[x, y] == TileType.Floor)
-        //        {
-        //            foreach (var dir in directions)
-        //            {
-        //                int nx = x + dir.x;
-        //                int ny = y + dir.y;
-
-        //                if (mapData[nx, ny] == TileType.Empty)
-        //                {
-        //                    mapData[nx, ny] = TileType.Wall;
-        //                    Vector3 pos = new Vector3(bounds.x + nx, 1, bounds.y + ny);
-        //                    GameObject.Instantiate(wallPrefab, pos, Quaternion.identity, parent);
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
 
         // 비어있는 구역은 임의의 벽과 땅으로 메우기
         for (int x = 0; x < width; x++)
@@ -85,16 +89,48 @@ public static class MapGenerator
                 if (mapData[x, y] == TileType.Empty)
                 {
                     Vector3 pos = new Vector3(bounds.x + x, 0, bounds.y + y);
-                    GameObject.Instantiate(floorPrefab, pos, Quaternion.identity, parent);
-                    var wallY = Vector3.up * (wallPrefab.transform.localScale.y / 2 + floorPrefab.transform.localScale.y / 2);
-                    GameObject.Instantiate(wallPrefab, pos + wallY, Quaternion.identity, parent);
+                    GameObject.Instantiate(FloorPrefab, pos, Quaternion.identity, parent);
+                    GameObject.Instantiate(WallPrefab, pos + WallOffset, Quaternion.identity, parent);
+                    mapData[x, y] = TileType.Wall;
                 }
             }
         }
 
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector3 pos = new Vector3(bounds.x + x, 0, bounds.y + y);
+                CreatePillar(mapData, x, y, pos, parent);
+            }
+        }
     }
 
-    private static void ConnectRooms(BSPNode node, TileType[,] mapData)
+
+    private void CreatePillar(TileType[,] map, int x, int y, Vector3 wallPos, Transform parent)
+    {
+        // 1x1부터 좌상단 4개의 블럭을 검사하도록
+        if (x < 1 || y > map.GetLength(1) - 2) return;
+
+        // 규칙: [좌,상,좌상] , [상], [좌]에 블럭이 있는 경우를 제외하면 좌상단 모서리에 기둥 필요
+        // 예외: x,y에 벽이 없는 경우는 위의 경우에서 기둥 필요
+        // 예외2: 좌상단만 벽이 있는 경우, x,y의 벽 유무와 상관없이 기둥 필요
+        bool left = map[x-1, y] == TileType.Wall;
+        bool up = map[x, y + 1] == TileType.Wall;
+        bool leftUp = map[x - 1, y + 1] == TileType.Wall;
+        bool exceptions = (left && up && leftUp) || (!left && up && !leftUp) || (left && !up && !leftUp);
+        bool exceptions2 = !left && !up && leftUp;
+
+        bool res = exceptions ^ (map[x, y] == TileType.Wall);
+        res |= exceptions2;
+        if (res)
+        {
+            var pillarPos = wallPos + PillarOffset;
+            GameObject.Instantiate(PillarPrefab, pillarPos, Quaternion.identity, parent);
+        }
+    }
+
+    private void ConnectRooms(BSPNode node, TileType[,] mapData)
     {
         if (node.IsLeaf) return;
 
@@ -120,96 +156,75 @@ public static class MapGenerator
         ConnectRooms(node.Right, mapData);
     }
 
-    public static void ConnectChunks(RectInt a, RectInt b, Transform parent, GameObject floorPrefab)
+    public bool TryConnectChunks(RectInt a, RectInt b, Transform parent)
     {
+        var res = false;
         // 두 청크가 수평/수직으로 인접한 경우에만 연결
-        if (a.Overlaps(b)) return;
-        Vector2Int pointA = GetEdgeConnectionPoint(a, b);
-        Vector2Int pointB = GetEdgeConnectionPoint(b, a);
+        if (a.Overlaps(b)) return res;
 
-        // 직선 통로
-        if (pointA.x == pointB.x)
-        {
-            for (int y = Mathf.Min(pointA.y, pointB.y); y <= Mathf.Max(pointA.y, pointB.y); y++)
-            {
-                Vector3 pos = new Vector3(pointA.x, 0, y);
-
-                if (Physics.Raycast(pos, Vector3.up, out var hit, 10, 1 << LayerMask.NameToLayer("Wall")))
-                {
-                    // 위쪽의 벽 허물기
-                    GameObject.Destroy(hit.collider.gameObject);
-                }
-            }
-        }
-        else if (pointA.y == pointB.y)
-        {
-            for (int x = Mathf.Min(pointA.x, pointB.x); x <= Mathf.Max(pointA.x, pointB.x); x++)
-            {
-                Vector3 pos = new Vector3(x, 0, pointA.y);
-                if (Physics.Raycast(pos, Vector3.up, out var hit, 10, 1 << LayerMask.NameToLayer("Wall")))
-                {
-                    // 위쪽의 벽 허물기
-                    GameObject.Destroy(hit.collider.gameObject);
-                }
-            }
-        }
-    }
-
-    private static Vector2Int GetEdgeConnectionPoint(RectInt from, RectInt to)
-    {
         // 가장자리 중 가까운 위치를 반환
         // 오른쪽
-        if (from.xMax == to.xMin)
+        if (a.xMax == b.xMin)
         {
-            int z = (int)Mathf.Clamp(to.center.y, from.yMin + 1, from.yMax - 2);
-            return GetSlimWallPoint(new Vector2Int(from.xMax - 1, z), Vector2Int.right, Vector2Int.up, 2, from.height);
+            int z = (int)Mathf.Clamp(b.center.y, a.yMin + 1, a.yMax - 2);
+            return TryBreakSlimWall(new Vector2Int(a.xMax - 1, z), Vector2Int.right, Vector2Int.up, 2, a.height);
         }
         // 왼쪽
-        if (from.xMin == to.xMax)
+        if (a.xMin == b.xMax)
         {
-            int z = (int)Mathf.Clamp(to.center.y, from.yMin + 1, from.yMax - 2);
-            return GetSlimWallPoint(new Vector2Int(from.xMin, z), Vector2Int.left, Vector2Int.up, 2, from.height);
+            int z = (int)Mathf.Clamp(b.center.y, a.yMin + 1, a.yMax - 2);
+            return TryBreakSlimWall(new Vector2Int(a.xMin, z), Vector2Int.left, Vector2Int.up, 2, a.height);
         }
         // 위쪽
-        if (from.yMax == to.yMin)
+        if (a.yMax == b.yMin)
         {
-            int x = (int)Mathf.Clamp(to.center.x, from.xMin + 1, from.xMax - 2);
-            return GetSlimWallPoint(new Vector2Int(x, from.yMax - 1), Vector2Int.up, Vector2Int.right, 2, from.width);
+            int x = (int)Mathf.Clamp(b.center.x, a.xMin + 1, a.xMax - 2);
+            return TryBreakSlimWall(new Vector2Int(x, a.yMax - 1), Vector2Int.up, Vector2Int.right, 2, a.width);
         }
         // 아래쪽
-        if (from.yMin == to.yMax)
+        if (a.yMin == b.yMax)
         {
-            int x = (int)Mathf.Clamp(to.center.x, from.xMin + 1, from.xMax - 2);
-            return GetSlimWallPoint(new Vector2Int(x, from.yMin), Vector2Int.down, Vector2Int.right, 2, from.width);
+            int x = (int)Mathf.Clamp(b.center.x, a.xMin + 1, a.xMax - 2);
+            return TryBreakSlimWall(new Vector2Int(x, a.yMin), Vector2Int.down, Vector2Int.right, 2, a.width);
         }
 
-        return Vector2Int.RoundToInt(from.center);
+        return res;
     }
 
-    private static Vector2Int GetSlimWallPoint(Vector2Int edge, Vector2Int turnelDir, Vector2Int wallDir, int offset, int maxLength)
+    private bool TryBreakSlimWall(Vector2Int edge, Vector2Int turnelDir, Vector2Int wallDir, int offset, int maxLength)
     {
         // 중앙부터 멀어지는 방향대로 벽 가장자리를 따라 조건에 부합하는 가장자리 좌표 확인
         // 일정 오프셋 뒤에서 터널 뚫는 방향으로 RaycastAll로 벽의 개수 검사
         for (int i = 0; i < maxLength / 2; i++)
         {
             // Cast시작할 좌표 (중앙에서 부터 멀어지는 방향으로 탐색)
-            var a = edge - offset * turnelDir + i * wallDir;
-            var startPos = new Vector3(a.x, 1, a.y);
-            var res = Physics.RaycastAll(startPos, new Vector3(turnelDir.x, 0, turnelDir.y), offset * 2 + 1, 1 << LayerMask.NameToLayer("Wall"));
-            // 벽이 2개인 경우에만 터널 뚫기
-            if (res.Length == 2) return edge + i * wallDir;
+            for (int j = 0; j < 2; j++)
+            {
+                var a = edge - offset * turnelDir + i * wallDir * (int)Mathf.Pow(-1,j);
+                var startPos = new Vector3(a.x, 1, a.y);
+                var rayDir = new Vector3(turnelDir.x, 0, turnelDir.y);
+                var res = Physics.RaycastAll(startPos, rayDir, offset * 2 + 1, 1 << LayerMask.NameToLayer("Wall"));
+                // 벽이 2개인 경우에만 터널 뚫기
+                if (res.Length == 2)
+                {
+                    //Debug.DrawRay(startPos, new Vector3(turnelDir.x, 0, turnelDir.y) * (offset * 2 + 1), Color.red, 3000f);
 
-            // 반대 방향
-            var b = edge - offset * turnelDir - i * wallDir;
-            var startPos2 = new Vector3(b.x, 1, b.y);
-            var res2 = Physics.RaycastAll(startPos, new Vector3(turnelDir.x, 0, turnelDir.y), offset * 2 + 1, 1 << LayerMask.NameToLayer("Wall"));
-            // 벽이 2개인 경우에만 터널 뚫기
-            if (res.Length == 2) return edge - i * wallDir;
-
-            //Debug.DrawRay(startPos, new Vector3(turnelDir.x, 0, turnelDir.y) * (offset * 2 + 1), Color.red, 3000f);
-            //Debug.DrawRay(startPos2, new Vector3(turnelDir.x, 0, turnelDir.y) * (offset * 2 + 1), Color.blue, 3000f);
+                    foreach (var obj in res)
+                    {
+                        Destroy(obj.collider.gameObject);
+                    }
+                    // 없어진 자리에는 아치형 문 생성
+                    // 회전 및 축 보정
+                    Quaternion rotation = Quaternion.LookRotation(rayDir);
+                    Vector3 rotatedOffset = rotation * GateOffset;
+                    GameObject.Instantiate(GatePrefab, startPos + rotatedOffset, rotation);
+                    GameObject.Instantiate(GatePrefab, startPos + rotatedOffset + rayDir * 2f, rotation);
+                    return true;
+                }
+            }
         }
-
-        return edge;
+        return false;
     }
+
+
 }
