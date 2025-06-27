4ebfd4ac3c504dac9a7dc91488cf593b27cc24d3
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-06-27 20:45:35 +0900

    [Feat] 카메라 타겟 추가 맟 프로젝트 설정 변경
    - (임시) 카메라 타겟 추가
    - 프로젝트 설정 변경
    - 절차적 맵 생성 씬 조정

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+    }
+}
