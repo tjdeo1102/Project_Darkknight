f070e10efcd57fd8915bd5c157eb62b1a7302cf4
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-07-22 23:44:36 +0900

    [Feat] 보스 구현
    - 보스 등장 관련 타임라인 구현 + 시네마 전용 보스 프리팹 생성
    - 보스 스킬 추가 (Throw, Jump)
    - 보스 전용 Animator, BT 구성 + 보스 랜덤 스킬 사용 액션 구현

---
+++ Assets/Scripts/Enemy/BT/Actions/RandomUseSkillAction.cs
@@ -0,0 +1,33 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+using Random = UnityEngine.Random;
+using System.Collections.Generic;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "RandomUseSkill", story: "[animator] set Rrandom [SkillParameter] in [MaxCount]", category: "Action", id: "bc2daad3a997915b145121bff133d195")]
+public partial class RandomUseSkillAction : Action
+{
+    [SerializeReference] public BlackboardVariable<Animator> animator;
+    [SerializeReference] public BlackboardVariable<string> SkillParameter;
+    [SerializeReference] public BlackboardVariable<int> MaxCount;
+    [SerializeReference] public BlackboardVariable<List<float>> SkillDelayList;
+    [SerializeReference] public BlackboardVariable<float> SkillDelay;
+
+    private int paramHash;
+    protected override Status OnStart()
+    {
+        paramHash = Animator.StringToHash(SkillParameter.Value);
+
+        var sel = Random.Range(0, MaxCount.Value);
+        if (sel < SkillDelayList.Value.Count) SkillDelay.Value = SkillDelayList.Value[sel];
+        else SkillDelay.Value = 0.2f;
+
+        animator.Value.SetInteger(paramHash, sel);
+
+        return Status.Running;
+    }
+}
+
