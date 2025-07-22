5c9a6a57a923cb13b982be25fca7ff6149eb8dab
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-07-22 23:31:50 +0900

    [Feat] BT 필요 액션 구현 및 수정, BT 그래프 수정
    - PatrolPathFinder :Enemy의 Nav 경로를 재탐색하는 액션 로직
    - SetVariableInitAction: BT관련 블랙보드 변수 자동 설정 액션 로직
    - ListCountCondition: 리스트의 크기 판단하는 조건 로직

--- Assets/Scripts/Enemy/BT/Actions/EnemyAnimatorControllerAction.cs
+++ Assets/Scripts/Enemy/BT/Actions/EnemyAnimatorControllerAction.cs
@@ -5,14 +5,14 @@ using Action = Unity.Behavior.Action;
 using Unity.Properties;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "EnemyAnimatorController", story: "Set [AnimationState] in [Animator] to [currentType]", category: "Action", id: "52c8c9f365c3bcf1d21c1b514a954f44")]
+[NodeDescription(name: "EnemyAnimatorController", story: "[animator] Set [AnimationState] to [currentType]", category: "Action", id: "52c8c9f365c3bcf1d21c1b514a954f44")]
 public partial class EnemyAnimatorControllerAction : Action
 {
+    [SerializeReference] public BlackboardVariable<Animator> animator;
     [SerializeReference] public BlackboardVariable<string> AnimationState;
-    [SerializeReference] public BlackboardVariable<Animator> EnemyAnimator;
     [SerializeReference] public BlackboardVariable<EnemyStateType> CurrentType;
-
     private int stateHash = -1;
+
     protected override Status OnStart()
     {
         stateHash = Animator.StringToHash(AnimationState.Value);
@@ -21,9 +21,9 @@ public partial class EnemyAnimatorControllerAction : Action
 
     protected override Status OnUpdate()
     {
-        if (EnemyAnimator != null)
+        if (animator.Value != null)
         {
-            EnemyAnimator.Value.SetInteger(stateHash, (int)CurrentType.Value);
+            animator.Value.SetInteger(stateHash, (int)CurrentType.Value);
         }
         return Status.Success;
     }
---
+++ Assets/Scripts/Enemy/BT/Actions/PatrolPathFinderAction.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections.Generic;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+using UnityEngine.AI;
+using System.Collections;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "PatrolPathFinder", story: "[Agent] finds [PatrolPoints] [Target] [IsActiveNavAgent]", category: "Action", id: "9674cb5a61a9ebf7be683bc6445e5da2")]
+public partial class PatrolPathFinderAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Agent;
+    [SerializeReference] public BlackboardVariable<List<GameObject>> PatrolPoints;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+    [SerializeReference] public BlackboardVariable<bool> IsActiveNavAgent;
+    [SerializeReference] public BlackboardVariable<float> RePathTime;
+    [SerializeReference] public BlackboardVariable<int> PatrolPointCount;
+    [SerializeReference] public BlackboardVariable<float> PatrolPointDistance;
+    [SerializeReference] public BlackboardVariable<bool> IsExitRoutine;
+
+    private NavMeshAgent m_navAgent;
+    private ObjectPool<Transform> m_patrolPool;
+    [CreateProperty] private float m_Timer = 5f;
+
+    protected override Status OnStart()
+    {
+        Init();
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        if (IsExitRoutine.Value)
+            return Status.Success;
+
+        if (IsTimeOut())
+        {
+            PathFind();
+        }
+
+        return Status.Running;
+    }
+
+    protected override void OnEnd()
+    {
+        base.OnEnd();
+
+        m_navAgent.enabled = false;
+        IsActiveNavAgent.Value = false;
+    }
+
+    private bool IsTimeOut()
+    {
+        m_Timer -= Time.deltaTime;
+        if (m_Timer < 0f)
+        {
+            m_Timer = RePathTime;
+            return true;
+        }
+        else return false;
+    }
+
+    public void Init()
+    {
+        m_navAgent = Agent.Value.GetComponentInChildren<NavMeshAgent>();
+        m_patrolPool = new();
+        var obj = new GameObject("point").transform;
+        obj.transform.parent = Agent.Value.transform;
+        m_patrolPool.poolObj = obj;
+        m_patrolPool.InitSize = PatrolPointCount;
+        m_patrolPool.Init(Agent.Value.transform);
+
+        if (InGameLoop.Instance != null)
+            Target.Value = InGameLoop.Instance.Player.gameObject;
+    }
+
+    public void Setup(List<GameObject> patrolPoints)
+    {
+        if (m_navAgent == null) return;
+        m_navAgent.updateRotation = true;
+        m_navAgent.updateUpAxis = true;
+
+        PatrolPoints.Value = patrolPoints;
+    }
+
+    public GameObject CreatePatrolPoint()
+    {
+        Vector3 ranPos = UnityEngine.Random.insideUnitCircle * PatrolPointDistance;
+        ranPos += Agent.Value.transform.position;
+        if (NavMesh.SamplePosition(ranPos, out NavMeshHit hit, PatrolPointDistance, NavMesh.AllAreas))
+        {
+            var obj = m_patrolPool.GetObject();
+            obj.position = hit.position;
+            return obj.gameObject;
+        }
+        return null;
+    }
+
+    public void PathFind()
+    {
+        var patrolPoints = new List<GameObject>();
+        // Check if Agent is currently placed on valid NavMesh Path
+        if (m_navAgent != null && IsActiveNavAgent == true && m_navAgent.isOnNavMesh)
+        {
+            if (patrolPoints.Count < 1)
+            {
+                patrolPoints = new List<GameObject>();
+
+                for (int i = 0; i < 4; i++)
+                {
+                    var obj = CreatePatrolPoint();
+                    if (obj != null)
+                        patrolPoints.Add(obj);
+                }
+                Setup(patrolPoints);
+            }
+        }
+        // RePath & Check if Agent is currently placed on unvalid NavMesh Path
+        else
+        {
+            if (NavMesh.SamplePosition(Agent.Value.transform.position, out NavMeshHit originHit, 2.0f, NavMesh.AllAreas))
+            {
+                m_navAgent.enabled = false;
+                Agent.Value.transform.position = originHit.position;
+                m_navAgent.enabled = true;
+                IsActiveNavAgent.Value = true;
+
+                if (patrolPoints.Count < 1)
+                {
+                    patrolPoints = new List<GameObject>();
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        var obj = CreatePatrolPoint();
+                        if (obj != null)
+                            patrolPoints.Add(obj);
+                    }
+                    Setup(patrolPoints);
+                }
+            }
+            else
+            {
+                m_navAgent.enabled = false;
+                IsActiveNavAgent.Value = false;
+
+                //Debug.LogWarning($"Agent {Agent.Value.name} is not on NavMesh at {Agent.Value.transform.position}");
+
+                foreach (var obj in patrolPoints)
+                {
+                    m_patrolPool.ReturnObject(obj.transform);
+                }
+                patrolPoints.Clear();
+            }
+        }
+    }
+}
+
