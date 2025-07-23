656a81980b34d22d59d81382cbef38cd74297ec9
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-07-23 12:49:06 +0900

    [Fix] IWeapon 대신 공통 클래스로 대체
    - 중복되는 로직에 대한 추상화 적용
    - 추후, 다른 Weapon이 추가되어도 유연하게 수정가능하도록 구현
    - 공통 메서드에 대한 가상화 적용을 통해 전략 패턴 유지

--- Assets/Scripts/Player/Weapons/Knife.cs
+++ Assets/Scripts/Player/Weapons/Knife.cs
@@ -1,42 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Knife : MonoBehaviour, IWeapon
+public class Knife : WeaponBase
 {
-    public float comboTime;
-    public int MaxComboCount = 2;
-    public PlayerController ctrl;
-
-    private static readonly int m_attackCountParam = Animator.StringToHash("AttackCount");
-    private int m_attackCount;
-    private float m_lastAttackTime;
     private void Start()
     {
+        m_attackCountParam = Animator.StringToHash("AttackCount");
         if (ctrl == null)
         {
             Debug.LogError("No ctrl");
             enabled = false;
             return;
         }
-        AddWeapon();
+        ActiveWeapon(false);
+        AddWeapon(WeaponType.Knife);
         m_attackCount = 0;
     }
-    public void AddWeapon()
-    {
-        ctrl.combat.RegisterWeapon(WeaponType.Knife, this);
-    }
-
-    public void Attack()
-    {
-        if (Time.time - m_lastAttackTime > comboTime)
-        {
-            m_attackCount = 0;
-        }
-        else m_attackCount = (m_attackCount + 1) % MaxComboCount;
-
-        ctrl.animator.SetInteger(m_attackCountParam, m_attackCount);
-
-        m_lastAttackTime = Time.time;
-    }
-
 }
--- Assets/Scripts/Player/Weapons/Sword.cs
+++ Assets/Scripts/Player/Weapons/Sword.cs
@@ -1,36 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sword : MonoBehaviour, IWeapon
+public class Sword : WeaponBase
 {
-    public float comboTime;
-    public int MaxComboCount = 3;
-    public PlayerController ctrl;
-
-    private int m_attackCountParam = Animator.StringToHash("AttackCount");
-    private int m_attackCount;
-    private float m_lastAttackTime;
-    public void AddWeapon()
-    {
-        ctrl.combat.RegisterWeapon(WeaponType.Sword, this);
-    }
-
-    public void Attack()
-    {
-        if (Time.time - m_lastAttackTime > comboTime)
-        {
-            m_attackCount = 0;
-        }
-        else m_attackCount = (m_attackCount + 1) % MaxComboCount;
-
-        ctrl.animator.SetInteger(m_attackCountParam, m_attackCount);
-
-        m_lastAttackTime = Time.time;
-    }
-
     private void Start()
     {
-        AddWeapon();
+        m_attackCountParam = Animator.StringToHash("AttackCount");
+        if (ctrl == null)
+        {
+            Debug.LogError("No ctrl");
+            enabled = false;
+            return;
+        }
+        ActiveWeapon(false);
+        AddWeapon(WeaponType.Sword);
         m_attackCount = 0;
     }
 }
