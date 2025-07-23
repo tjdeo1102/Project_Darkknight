9a2644489d2eac9e9405949166064d0f39a72fd8
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-07-23 12:59:02 +0900

    [Fix] SkillBase 수정
    - 데미지 포함 모든 스탯수치는 SkillStats로 관리할 수 있도록 추상화
    - 스킬 사용 조건 로직 수정
    - 피격 판정 관련 로직 추가

--- Assets/Scripts/Player/Skill/ProjectileSkill.cs
+++ Assets/Scripts/Player/Skill/ProjectileSkill.cs
@@ -1,41 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.NetworkInformation;
 using Unity.Cinemachine;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ProjectileSkill", menuName = "Scriptable Objects/Projectile Skill")]
-public class ProjectileSkill: SkillBase
+public class ProjectileSkill : SkillBase
 {
-    public ObjectPool<Bullet> BulletPool;
+    public ProjectileType ProjectileType;
     public float Speed;
     public float LifeTime;
-    public float Damage;
+    public float KnockBackForce = 1f;
 
-    private WaitForSeconds m_delay;
-
-    public override void OnEnable()
-    {
-        base.OnEnable();
-        BulletPool.Init();
-        m_delay = new WaitForSeconds(LifeTime);
-    }
 
     public override IEnumerator Active(Transform origin, Dictionary<StatType, Stat> stats, GameObject Target)
-    { 
+    {
+        if (ProjectileManager.Instance == null || ProjectileType == ProjectileType.None) yield break;
         yield return base.Active(origin, stats, Target);
 
         if (isFailSkill == false && stats.TryGetValue(StatType.AttackPower, out var atk) == true)
         {
             origin.LookAt(Target.transform);
 
-            var bullet = BulletPool.GetObject();
+            var pool = ProjectileManager.Instance.ProjectileDic[ProjectileType];
+            var bullet = pool.GetObject();
             var trans = bullet.transform;
             trans.position = center;
             trans.rotation = origin.rotation;
 
+            // 딜레이로 인한 foward 갱신 필요
+            SetStartPos(origin);
             var velocity = foward * Speed;
-            bullet.Init(Damage + atk.TotalValue, LifeTime, velocity, TagManager.TryGetTargetTag(Target.tag) , BulletPool);
+
+            var Damage = 0f;
+            var atts = SkillStats.Where(skillStat => skillStat.StatType == StatType.AttackPower).ToArray();
+            if (atts.Length > 0)
+            {
+                var baseDamage = atts[0].StatModifier.FixedValue * (atts[0].StatModifier.PercentValue + 1);
+                Damage = atts[0].EnforceStatFactor * EnforceLevel + baseDamage;
+            }
+
+            bullet.Init(Damage + atk.TotalValue, LifeTime, velocity, KnockBackForce, TagManager.TryGetTargetTag(Target.tag), pool);
         }
     }
 }
--- Assets/Scripts/Player/Skill/SkillBase.cs
+++ Assets/Scripts/Player/Skill/SkillBase.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Rendering;
 using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "SkillBase", menuName = "Scriptable Objects/Skill Base")]
-public abstract class SkillBase : ScriptableObject
+public abstract class SkillBase : CSVScriptableObject
 {
     private static int skillAnimationParam = Animator.StringToHash("SkillType");
 
     [Header("CSV Parse")]
-    [CSVField(CSVFIledType.None)]
-    public string ID = "";
     [CSVField(CSVFIledType.None)]
     public string SkillName = "� ��ų";
     [CSVField(CSVFIledType.None)]
@@ -31,6 +31,8 @@ public abstract class SkillBase : ScriptableObject
     public bool CanActive = false;
     [CSVField(CSVFIledType.None)]
     public bool CanUnlock = false;
+    [CSVField(CSVFIledType.StatArr)]
+    public StatChange[] SkillStats;
 
     [Header("Unity Editor Setting")]
     public Sprite Icon;
@@ -43,8 +45,6 @@ public abstract class SkillBase : ScriptableObject
     public float ActiveDelay = 1f;
     public float EffectDelay = 0.5f;
 
-    private PlayerController m_player;
-    private Transform m_origin;
     private HashSet<SkillBase> m_lockRequireSkills;
     private float m_lastSkillUseTime;
     protected Vector3 center;
@@ -53,7 +53,6 @@ public abstract class SkillBase : ScriptableObject
     protected Vector3 up;
 
     protected bool isFailSkill;
-
 
 
     public virtual void OnEnable()
@@ -74,64 +73,62 @@ public abstract class SkillBase : ScriptableObject
         if (m_lockRequireSkills.Count < 1) CanUnlock = true;
     }
 
-    private bool CanUseSkill(Stat mp)
+    public bool CanUseSkill(Stat mp, bool currentUse = true)
     {
-        isFailSkill = false;
+        if (currentUse) isFailSkill = false;
         if (mp ==  null) mp = new Stat();
 
         if (Time.time - m_lastSkillUseTime < Cooldown
             || CostMP > mp.TotalValue)
         {
-            Debug.Log("no actgive");
-
-            isFailSkill = true;
+            if (currentUse) isFailSkill = true;
             return false;
         }
 
-        mp.AddModifier(new StatModifier(-CostMP, 0), StatModifyType.Perment);
-        m_lastSkillUseTime = Time.time;
+        if (currentUse)
+        {
+            mp.AddModifier(new StatModifier(-CostMP, 0), StatModifyType.SkillUse);
+            m_lastSkillUseTime = Time.time;
+        }
+
         return true;
     }
 
-    private void SetStartPos()
+    protected void SetStartPos(Transform origin)
     {
-        foward = m_origin.forward;
-        right = m_origin.right;
-        up = m_origin.up;
-        center = m_origin.position + foward * StartOffset.z + right * StartOffset.x + up * StartOffset.y;
+        foward = origin.forward;
+        right = origin.right;
+        up = origin.up;
+        center = origin.position + foward * StartOffset.z + right * StartOffset.x + up * StartOffset.y;
+        var navAgent = origin.GetComponentInParent<NavMeshAgent>();
+        if (navAgent != null)
+        {
+            navAgent.updateRotation = false;
+            origin.LookAt(center);
+            navAgent.updateRotation = true;
+        }
     }
 
-    private void SkillEffect()
+    private void SkillEffect(Transform origin, PlayerController player = null)
     {
         if (SkillType != VFX.None)
         {
-            if (m_player != null)
+            if (player != null)
             {
-                m_player.animator.SetInteger(skillAnimationParam, (int)SkillType);
-                m_player.machine.ChangeState(StateType.Skill);
+                player.animator.SetInteger(skillAnimationParam, (int)SkillType);
+                player.machine.ChangeState(StateType.Skill);
             }
-            SkillEffectManager.Instance.PlayVFX(SkillType, center, m_origin.rotation, EffectDelay);
+            SkillEffectManager.Instance.PlayVFX(SkillType, center, origin.rotation, EffectDelay);
         }
     }
 
     public virtual IEnumerator Active(PlayerController player)
     {
-        if (player != null)
+        if (player != null && player.model.Stats.TryGetValue(StatType.Mana, out var mp))
         {
-            this.m_player = player;
-            var mp = player.model.Stats[StatType.Mana];
-            m_origin = player.transform;
-
-            if (m_player.combat.CurType != RequireWeapon && RequireWeapon != WeaponType.None)
-            {
-                Debug.Log("전용무기 장착 후, 사용 필요");
-                isFailSkill = true;
-                yield break;
-            }
-
             if (CanUseSkill(mp) == false) yield break;
-            SetStartPos();
-            SkillEffect();
+            SetStartPos(player.transform);
+            SkillEffect(player.transform,player);
         }
         yield return new WaitForSeconds(ActiveDelay);
     }
@@ -140,11 +137,9 @@ public abstract class SkillBase : ScriptableObject
     {
         if (origin != null && stats.TryGetValue(StatType.Mana,out var mp))
         {
-            m_origin = origin;
             if (CanUseSkill(mp) == false) yield break;
-            Debug.Log("Active");
-            SetStartPos();
-            SkillEffect();
+            SetStartPos(origin);
+            SkillEffect(origin);
         }
 
         yield return new WaitForSeconds(ActiveDelay);
