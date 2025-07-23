49bb4e0fc3e5ab21fc4e79001392ee35db864850
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-07-23 13:12:17 +0900

    [Fix] UI 요소의 공통적 로직 추상화
    - 기존 인터페이스로 전부 대체할 수 없었던 공통 로직들을 추상화
    - 모든 UI 요소들은 UIElementBase에 의해 관리되며, UI Conroller가 이를 관리하도록 수정
    - UI Controller의 Dead, KillChange, Money Change에 대한 UI 추가 및 관리

---
+++ Assets/Scripts/UI/UIElementBase.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class UIElementBase : MonoBehaviour
+{
+    public UIController Controller;
+    public UIState Type;
+    public bool DependencyOnChangeState = true;
+
+    protected virtual void OnDisable()
+    {
+        Controller.ChangeState(UIState.None);
+    }
+}
