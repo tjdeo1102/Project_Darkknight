b522059fb2a552e2106b8bc03b2d3973294aa846
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-07-23 13:09:34 +0900

    [Fix] 스킬 사용 슬롯 View UI 추가

---
+++ Assets/Scripts/UI/SkillSlotView.cs
@@ -0,0 +1,59 @@
+using DG.Tweening;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillSlotView : MonoBehaviour
+{
+    [Serializable]
+    public struct SlotContent
+    {
+        public Image SlotImage;
+        public Animator Animator;
+    }
+
+    public SlotContent[] slotContents;
+    public PlayerCombat Combat;
+    private Stat m_mana;
+    private readonly int animParam = Animator.StringToHash("CanUse");
+
+    void Start()
+    {
+        if (InGameLoop.Instance != null && 
+            InGameLoop.Instance.Player != null)
+        {
+            var Ctrl = InGameLoop.Instance.Player;
+            Combat = Ctrl.combat;
+            m_mana = Ctrl.model.Mana;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RefreshSkillState();
+    }
+
+    public void RefreshSkillState()
+    {
+        if (Combat != null)
+        {
+            if (Combat.skills.Count != slotContents.Length) return;
+
+            var skills = Combat.skills;
+            for (int i = 0; i < skills.Count; i++)
+            {
+                if (skills[i] != null && skills[i].CanUseSkill(m_mana, false))
+                {
+                    slotContents[i].Animator?.SetBool(animParam, true);
+                    slotContents[i].SlotImage.color = Color.white;
+                }
+                else
+                {
+                    slotContents[i].Animator?.SetBool(animParam, false);
+                    slotContents[i].SlotImage.color = new Color(0.5f, 0.5f, 0.5f);
+                }
+            }
+        }
+    }
+}
